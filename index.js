4aab5b644321ccbc2dea90306e57494b5b24614c
officerkiruha <kirillkovalchukstudy@gmail.com>
2025-11-23 16:05:16 +0200

    add folder config and conect between everything

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 const api = process.env.HOST;
-
 app.use(express.static(__dirname));
-
+const db = require('./config/db_config.js')
 app.get('/',(req,res)=>{res.sendFile(__dirname+'/Public/index.html')});
 
 app.listen(port,()=>{console.log(`http://${api}:${port}`)});
