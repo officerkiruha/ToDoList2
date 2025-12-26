5b6c04e955c3c65277cc48428e27b7a8a1f18205
officerkiruha <kirillkovalchukstudy@gmail.com>
2025-12-26 09:37:42 +0200

    add html login and script

--- Public/scripts/login.js
+++ Public/scripts/login.js
@@ -1,17 +1,15 @@
-async function register(){
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
+async function login(){
     let userName = document.getElementById('userName').value;
     let pass = document.getElementById('pass').value;
     try{
-        if(name && email && userName && pass){
+        if(userName && pass){
         let response = await fetch('/auth/reg',{
             method:'POST',
             headers:{'Content-Type':'application/json'},
-            body:JSON.stringify({name,email,userName,pass})
+            body:JSON.stringify({userName,pass})
         })
-        if(response.status==201){
-            window.location.href = '/login';
+        if(response.status==200){
+            window.location.href = '/';
             return;
         }
              let data = await response.json();
--- routes/pages_R.js
+++ routes/pages_R.js
@@ -4,5 +4,6 @@ const path = require('path');
 
 router.get('/',(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","index.html"))});
 router.get('/register',(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","register.html"))});
+router.get('/login',(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","login.html"))});
 
 module.exports = router;
