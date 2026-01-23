8768d9acdc408c09fb7394f0518e0432239a0a79
officerkiruha <kirillkovalchukstudy@gmail.com>
2026-01-23 19:28:22 +0200

    add routes for categories and useres

--- routes/pages_R.js
+++ routes/pages_R.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { isLoggedIn } = require('../middelware/auth_MID');
 
-router.get('/',(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","index.html"))});
+router.get('/',isLoggedIn,(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","index.html"))});
 router.get('/register',(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","register.html"))});
 router.get('/login',(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","login.html"))});
-
+router.get('/categories',isLoggedIn,(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","categories.html"))});
+router.get('/users',isLoggedIn,(req,res)=>{res.sendFile(path.join(__dirname,"..","Public","pages","users.html"))});
 module.exports = router;
